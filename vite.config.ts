911bbd31dab302017c439509282c592d01fcbcd5
Tainan-Zart <tainan_zart@estudante.sesisenai.org.br>
2025-09-14 13:16:20 -0300

    Ajuste para rodar no github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/RotaRural/",
+  build: {
+    outDir: "docs", 
+  },
+
   server: {
     host: "::",
     port: 8080,
